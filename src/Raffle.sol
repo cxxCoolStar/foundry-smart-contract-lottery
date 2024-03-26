66014aba6952150143c4073933624fa15420fbbc
asta <1848505943@qq.com>
2024-03-26 19:43:49 +0800

    init

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,25 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 /**
@@ -8,19 +29,26 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+    error Raffle_NotEnoughETHSend();
+
     uint256 private immutable i_entranceFee;
+    address [] private s_players
 
     constructor(uint256 enterRaffle) {
         i_entranceFee = enterRaffle;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughETHSend();
+        }
+    }
 
     function pickWinner() public {}
 
     /** Getter Function */
 
-    function getEntranceFee() external view returns(uint256) {
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }
